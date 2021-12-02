a93094e3a711ba1a69f97036cb870bec79771f34
moukmessie <moukmessie@gmail.com>
2021-12-02 23:59:33 +0100

    authentication whith Laravel Breeze

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except('bar');
+    }
+
+    public function foo()
+    {
+        return view('test/foo');
+    }
+    public function bar()
+    {
+        return view('test/bar');
+    }
+}
